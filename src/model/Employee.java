287e13b7ecdcb43b188ad5d0dd8fd5f3a6d479cc
Xcoder <chindaebube03@gmail.com>
2022-09-05 20:02:38 +0100

    Added fields for employee class

--- src/model/Employee.java
+++ src/model/Employee.java
@@ -2,5 +2,69 @@ package model;
 
 public class Employee {
     int id;
-    String
+    String firstname;
+    String lastname;
+    String DOB;
+    String email;
+    String address;
+
+    public Employee() {
+    }
+
+    public Employee(int id, String firstname, String lastname, String DOB, String email, String address) {
+        this.id = id;
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.DOB = DOB;
+        this.email = email;
+        this.address = address;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getDOB() {
+        return DOB;
+    }
+
+    public void setDOB(String DOB) {
+        this.DOB = DOB;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
 }
