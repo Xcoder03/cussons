f1a0522e45e47e71d0aaa9b66d7fd19998c74be1
Xcoder <chindaebube03@gmail.com>
2022-09-05 19:59:31 +0100

    Added fields for hygiene  produce class

--- src/model/HygieneProduce.java
+++ src/model/HygieneProduce.java
@@ -1,4 +1,70 @@
 package model;
 
 public class HygieneProduce {
+    int id;
+    String productName;
+    int quantity;
+    String productionDate;
+    String ExpirationDate;
+    int price;
+
+    public HygieneProduce() {
+    }
+
+    public HygieneProduce(int id, String productName, int quantity, String productionDate, String expirationDate, int price) {
+        this.id = id;
+        this.productName = productName;
+        this.quantity = quantity;
+        this.productionDate = productionDate;
+        ExpirationDate = expirationDate;
+        this.price = price;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getProductionDate() {
+        return productionDate;
+    }
+
+    public void setProductionDate(String productionDate) {
+        this.productionDate = productionDate;
+    }
+
+    public String getExpirationDate() {
+        return ExpirationDate;
+    }
+
+    public void setExpirationDate(String expirationDate) {
+        ExpirationDate = expirationDate;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
 }
