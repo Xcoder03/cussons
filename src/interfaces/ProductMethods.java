f77605bbde162b72f13a85fe4d8ff779ea5d5a67
Xcoder <chindaebube03@gmail.com>
2022-09-09 13:40:36 +0100

    created product methods

--- src/interfaces/ProductMethods.java
+++ src/interfaces/ProductMethods.java
@@ -1,4 +1,9 @@
 package interfaces;
 
+import model.Produce;
+
 public interface ProductMethods {
+  public  boolean uploadBabyProduct(Produce produce);
+  public  boolean uploadBeautyProduct(Produce produce);
+    public  boolean uploadHygieneProduct(Produce produce);
 }
